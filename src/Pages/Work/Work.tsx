4c7b8748e245589aaa51cad850e5fde10675881e
Kangseohyeon <kshyeon123@gmail.com>
2023-04-18 16:08:15 +0900

    adding images

--- src/Pages/Work/Work.tsx
+++ src/Pages/Work/Work.tsx
@@ -4,11 +4,31 @@ import { useNavigate } from "react-router-dom";
 const Work: React.FC = () => {
   const navigate = useNavigate();
   return (
-    <div className="w-full h-full">
-      <div onClick={() => {
-        navigate("/detail/1")
-      }}>
-        Work 1
+    <div className="flex w-full h-full">
+      <div className="w-1/2 h-full">
+
+      </div>
+      <div className="flex items-center justify-center w-1/2 h-full">
+        <div className="">
+          <div>
+            HAVAH Mitter
+        </div>
+          <div>
+            HAVAH Planet Runner
+        </div>
+          <div>
+            HAVAH Scan
+        </div>
+          <div>
+            Partner Center
+        </div>
+          <div>
+            Zzeung
+        </div>
+          <div>
+            Jeju Safety QR
+        </div>
+        </div>
       </div>
     </div>
   )
